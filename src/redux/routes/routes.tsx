703779dfcf3145893ea4d19f5745f6396511da0a
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-06 01:51:41 +0800

    feat: add pay success url

--- src/redux/routes/routes.tsx
+++ src/redux/routes/routes.tsx
@@ -1,12 +1,17 @@
 import Home from "@/page/home/Home";
+import PaySuccess from "@/page/pay/success/PaySuccess";
 import { createBrowserRouter } from "react-router-dom";
 
 
 const routes = createBrowserRouter([
     {
         path: "/",
-        element: <Home/>
-    }
+        element: <Home />
+    },
+    {
+        path: "/product/pay/success",
+        element: <PaySuccess />
+    },
 ]);
 
 export default routes;
