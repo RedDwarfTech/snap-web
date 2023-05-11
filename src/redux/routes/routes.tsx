60d3390a210921e8c94b49525bd5e0ecd21b2719
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-05-11 22:55:32 +0800

    feat: replace the pay success page

--- src/redux/routes/routes.tsx
+++ src/redux/routes/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "@/page/home/Home";
-import PaySuccess from "@/page/pay/success/PaySuccess";
+import { PaySuccess } from "rd-component";
 
 const routes = createBrowserRouter([
     {
